31b4a92c29b9eadfc915c04853b508008cdc07aa
Werner Chao <oahcrenrew@gmail.com>
2016-12-13 21:35:44 -0500

    simplified transformation step with loop

--- poly-house-price.py
+++ poly-house-price.py
@@ -52,86 +52,88 @@ mean_lat_plus_long = np.mean(test["lat_plus_long"])
 
 # Step (4) aggregating feature columns for modelling
 # Model_1, train & test
-model_1_train = train[list(train.columns[3:6]) + list(train.columns[17:19])]
-model_1_test = test[list(test.columns[3:6]) + list(test.columns[17:19])]
-model_2_train = train[list(train.columns[3:6]) + list(train.columns[17:19]) + list(train.columns[22:23])]
-model_2_test = test[list(test.columns[3:6]) + list(test.columns[17:19]) + list(test.columns[22:23])]
-model_3_train = train[list(train.columns[3:6]) + list(train.columns[17:19]) + list(train.columns[21:25])]
-model_3_test = test[list(test.columns[3:6]) + list(test.columns[17:19]) + list(test.columns[21:25])]
+model = {}
+model["model_1_train"] = train[list(train.columns[3:6]) + list(train.columns[17:19])]
+model["model_1_test"] = test[list(test.columns[3:6]) + list(test.columns[17:19])]
+model["model_2_train"] = train[list(train.columns[3:6]) + list(train.columns[17:19]) + list(train.columns[22:23])]
+model["model_2_test"] = test[list(test.columns[3:6]) + list(test.columns[17:19]) + list(test.columns[22:23])]
+model["model_3_train"] = train[list(train.columns[3:6]) + list(train.columns[17:19]) + list(train.columns[21:25])]
+model["model_3_test"] = test[list(test.columns[3:6]) + list(test.columns[17:19]) + list(test.columns[21:25])]
+
+for i in range(1, 3):
+    
+    poly = PolynomialFeatures(degree=i)
+    d_poly = {}
+    for x in range(1,4):
+        # Step (4.5) polynomial feature transformation
+        d_poly["poly_model_{0}_train".format(x)]=poly.fit_transform(model["model_{0}_train".format(x)])
+        d_poly["poly_model_{0}_test".format(x)]=poly.fit_transform(model["model_{0}_test".format(x)])
+        print d_poly["poly_model_1_test"].shape
+
+        # lr[{0}.format(x)] = linear_model.LinearRegression()
+        # lr[{0}.format(x)].fit(model["model_1_train"], train['price'])
+
+    # Step (5) fitting and predicting
+    lr1 = linear_model.LinearRegression()
+    lr1.fit(model["model_1_train"], train['price'])
+    # print lr1.coef_
+
+    lr1_poly = linear_model.LinearRegression()
+    lr1_poly.fit(d_poly["poly_model_1_train"], train['price'])
+    # print lr1_poly.coef_
+
+    lr2 = linear_model.LinearRegression()
+    lr2.fit(model["model_2_train"], train['price'])
+    # print lr2.coef_
+
+    lr2_poly = linear_model.LinearRegression()
+    lr2_poly.fit(d_poly["poly_model_2_train"], train['price'])
+    # print lr2_poly.coef_
+
+    lr3 = linear_model.LinearRegression()
+    lr3.fit(model["model_3_train"], train['price'])
+    # print lr3.coef_
+
+    lr3_poly = linear_model.LinearRegression()
+    lr3_poly.fit(d_poly["poly_model_3_train"], train['price'])
+    # print lr3_poly.coef_
 
 
-# Step (4.5) polynomial feature transformation
-poly = PolynomialFeatures(degree=2)
-poly_model_1_train = poly.fit_transform(model_1_train)
-poly_model_1_test = poly.fit_transform(model_1_test)
-poly_model_2_train = poly.fit_transform(model_2_train)
-poly_model_2_test = poly.fit_transform(model_2_test)
-poly_model_3_train = poly.fit_transform(model_3_train)
-poly_model_3_test = poly.fit_transform(model_3_test)
-print poly_model_1_test.shape
+    degree = i
+    pred1 = lr1.predict(model["model_1_test"])
+    mse1 = mean_squared_error(test['price'], pred1)
+    pred1_poly = lr1_poly.predict(d_poly["poly_model_1_test"])
+    mse1_poly = mean_squared_error(test['price'], pred1_poly)
+    print "Printing MSE of both model_1 (degree=%s): " %degree
+    print mse1, mse1_poly
 
+    pred2 = lr2.predict(model["model_2_test"])
+    mse2 = mean_squared_error(test['price'], pred2)
+    pred2_poly = lr2_poly.predict(d_poly["poly_model_2_test"])
+    mse2_poly = mean_squared_error(test['price'], pred2_poly)
+    print "Printing MSE of both model_2 (degree=%s): " %degree
+    print mse2, mse2_poly
 
-# Step (5) fitting and predicting
-lr1 = linear_model.LinearRegression()
-lr1.fit(model_1_train, train['price'])
-# print lr1.coef_
-
-lr1_poly = linear_model.LinearRegression()
-lr1_poly.fit(poly_model_1_train, train['price'])
-# print lr1_poly.coef_
-
-lr2 = linear_model.LinearRegression()
-lr2.fit(model_2_train, train['price'])
-# print lr2.coef_
-
-lr2_poly = linear_model.LinearRegression()
-lr2_poly.fit(poly_model_2_train, train['price'])
-# print lr2_poly.coef_
-
-lr3 = linear_model.LinearRegression()
-lr3.fit(model_3_train, train['price'])
-# print lr3.coef_
-
-lr3_poly = linear_model.LinearRegression()
-lr3_poly.fit(poly_model_3_train, train['price'])
-# print lr3_poly.coef_
+    pred3 = lr3.predict(model["model_3_test"])
+    mse3 = mean_squared_error(test['price'], pred3)
+    pred3_poly = lr3_poly.predict(d_poly["poly_model_3_test"])
+    mse3_poly = mean_squared_error(test['price'], pred3_poly)
+    print "Printing MSE of both model_3 (degree=%s): " %degree
+    print mse3, mse3_poly
 
 
 
-pred1 = lr1.predict(model_1_test)
-mse1 = mean_squared_error(test['price'], pred1)
-pred1_poly = lr1_poly.predict(poly_model_1_test)
-mse1_poly = mean_squared_error(test['price'], pred1_poly)
-print "Printing MSE of both model_1: "
-print mse1, mse1_poly
+    score1 = lr1.score(model["model_1_test"], test['price'])
+    score1_poly = lr1_poly.score(d_poly["poly_model_1_test"], test['price'])
+    print "Printing SCORE of both model_1 (degree=%s):" %degree
+    print score1, score1_poly
 
-pred2 = lr2.predict(model_2_test)
-mse2 = mean_squared_error(test['price'], pred2)
-pred2_poly = lr2_poly.predict(poly_model_2_test)
-mse2_poly = mean_squared_error(test['price'], pred2_poly)
-print "Printing MSE of both model_2: "
-print mse2, mse2_poly
+    score2 = lr2.score(model["model_2_test"], test['price'])
+    score2_poly = lr2_poly.score(d_poly["poly_model_2_test"], test['price'])
+    print "Printing SCORE of both model_2 (degree=%s):" %degree
+    print score2, score2_poly
 
-pred3 = lr3.predict(model_3_test)
-mse3 = mean_squared_error(test['price'], pred3)
-pred3_poly = lr3_poly.predict(poly_model_3_test)
-mse3_poly = mean_squared_error(test['price'], pred3_poly)
-print "Printing MSE of both model_3: "
-print mse3, mse3_poly
-
-
-
-score1 = lr1.score(model_1_test, test['price'])
-score1_poly = lr1_poly.score(poly_model_1_test, test['price'])
-print "Printing SCORE of both model_1:"
-print score1, score1_poly
-
-score2 = lr2.score(model_2_test, test['price'])
-score2_poly = lr2_poly.score(poly_model_2_test, test['price'])
-print "Printing SCORE of both model_2:"
-print score2, score2_poly
-
-score3 = lr3.score(model_3_test, test['price'])
-score3_poly = lr3_poly.score(poly_model_3_test, test['price'])
-print "Printing SCORE of both model_3:"
-print score3, score3_poly
+    score3 = lr3.score(model["model_3_test"], test['price'])
+    score3_poly = lr3_poly.score(d_poly["poly_model_3_test"], test['price'])
+    print "Printing SCORE of both model_3 (degree=%s):" %degree
+    print score3, score3_poly
